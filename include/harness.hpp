3b5f0fc6e21f4af039b508bdf16b15d7d375beb5
Prateek Chawla <p.chawla@fz-juelich.de>
2025-11-06 09:50:41 +0100

    modified harness

--- include/harness.hpp
+++ include/harness.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
-#include <string>
-#include <vector>
-#include <ranges>
-#include <vector>
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include "utils.hpp"
-#include "profiler.hpp"
+#include <ranges>
+#include <string>
+#include <vector>
 
-struct BenchmarkResult
-{
+#include "profiler.hpp"
+#include "utils.hpp"
+
+struct BenchmarkResult {
     int m;
     int n;
     int k;
@@ -26,26 +25,27 @@ struct BenchmarkResult
 };
 
 template <typename T>
-using GemmFunctionPointer = void (*)(int, int, int, T, const std::vector<T> &, const std::vector<T> &, T, std::vector<T> &);
+using GemmFunctionPointer = void (*)(int, int, int, T, const std::vector<T>&, const std::vector<T>&, T, std::vector<T>&);
 
 template <typename T>
-class Harness
-{
+class Harness {
     // test  harness to run a gemm test
 
-public:
+   public:
     Harness() { m_monitor = new PerfEvent(); }
-    Harness(int _m, int _n, int _k, int _runs, T _alpha, T _beta, GemmFunctionPointer<T> _funcPtr, std::string _label) : m(_m), n(_n), k(_k), runs(_runs), alpha(_alpha), beta(_beta), funcPtr(_funcPtr), test_label(_label) { m_monitor = new PerfEvent(); }
+    Harness(int _m, int _n, int _k, int _runs, T _alpha, T _beta, GemmFunctionPointer<T> _funcPtr, std::string _label)
+        : m(_m), n(_n), k(_k), runs(_runs), alpha(_alpha), beta(_beta), funcPtr(_funcPtr), test_label(_label) {
+        m_monitor = new PerfEvent();
+    }
     BenchmarkResult RunBenchmark();
 
-    ~Harness()
-    {
+    ~Harness() {
         // release memory for the monitor instance
         delete m_monitor;
         m_monitor = nullptr;
     }
 
-protected:
+   protected:
     int m = 1;
     int n = 1;
     int k = 1;
@@ -55,13 +55,12 @@ protected:
     T beta = 0.0;
     GemmFunctionPointer<T> funcPtr;
     std::string test_label;
-    PerfEvent *m_monitor;
+    PerfEvent* m_monitor;
 };
 
 // need to include templated functions in the header itself
 template <typename T>
-BenchmarkResult Harness<T>::RunBenchmark()
-{
+BenchmarkResult Harness<T>::RunBenchmark() {
     std::clog << "Running benchmark for " << test_label << std::endl;
     // we already have initialized members, we need to get the test harness ready
     std::vector<T> A = generate_random_matrix<T>(m, k);
@@ -71,10 +70,8 @@ BenchmarkResult Harness<T>::RunBenchmark()
     std::vector<std::vector<EventType>> monitor_reports;
     std::vector<long double> timeDurations;
 
-    for (auto r : std::views::iota(0, runs + warmupRuns))
-    {
-        if (r > 0)
-        {
+    for (auto r : std::views::iota(0, runs + warmupRuns)) {
+        if (r > 0) {
             A = generate_random_matrix<T>(m, k);
             B = generate_random_matrix<T>(k, n);
             C = generate_random_matrix<T>(m, n);
@@ -105,55 +102,53 @@ BenchmarkResult Harness<T>::RunBenchmark()
 }
 
 template <typename T>
-using KokkosGemmPtr = void (*)(int, int, int, T, const Kokkos::View<T **> &A, const Kokkos::View<T **> &B, T beta, Kokkos::View<T **> &C);
+using KokkosGemmPtr = void (*)(int, int, int, T, const Kokkos::View<T**>& A, const Kokkos::View<T**>& B, T beta, Kokkos::View<T**>& C);
 
 template <typename T>
-class KokkosHarness : public Harness<T>
-{
+class KokkosHarness : public Harness<T> {
     // test  harness to run a gemm test
 
-public:
+   public:
     KokkosHarness() { m_monitor = new PerfEvent(); }
-    KokkosHarness(int _m, int _n, int _k, int _runs, T _alpha, T _beta, KokkosGemmPtr<T> _funcPtr, std::string _label) : m(_m), n(_n), k(_k), runs(_runs), alpha(_alpha), beta(_beta), funcPtr(_funcPtr), test_label(_label) { m_monitor = new PerfEvent(); }
+    KokkosHarness(int _m, int _n, int _k, int _runs, int _warmupRuns, T _alpha, T _beta, KokkosGemmPtr<T> _funcPtr, std::string _label)
+        : m(_m), n(_n), k(_k), runs(_runs), warmupRuns(_warmupRuns), alpha(_alpha), beta(_beta), funcPtr(_funcPtr), test_label(_label) {
+        m_monitor = new PerfEvent();
+    }
     BenchmarkResult RunBenchmark();
 
-    ~KokkosHarness()
-    {
+    ~KokkosHarness() {
         // release memory for the monitor instance
         delete m_monitor;
         m_monitor = nullptr;
     }
 
-protected:
+   protected:
     int m = 1;
     int n = 1;
     int k = 1;
     int runs = 1;
-    int warmupRuns = 20;
+    int warmupRuns = 1;
     T alpha = 0.0;
     T beta = 0.0;
     KokkosGemmPtr<T> funcPtr;
     std::string test_label;
-    PerfEvent *m_monitor;
+    PerfEvent* m_monitor;
 };
 
 // need to include templated functions in the header itself
 template <typename T>
-BenchmarkResult KokkosHarness<T>::RunBenchmark()
-{
+BenchmarkResult KokkosHarness<T>::RunBenchmark() {
     // we already have initialized members, we need to get the test harness ready
-    Kokkos::View<float **> A("A", m, k);
-    Kokkos::View<float **> B("B", k, n);
-    Kokkos::View<float **> C("C", m, n);
+    Kokkos::View<float**> A("A", m, k);
+    Kokkos::View<float**> B("B", k, n);
+    Kokkos::View<float**> C("C", m, n);
 
     std::vector<std::vector<EventType>> monitor_reports;
     std::vector<long double> timeDurations;
 
     std::clog << "Running benchmark for " << test_label << std::endl;
-    for (auto r : std::views::iota(0, runs + warmupRuns))
-    {
-        if (r > 0)
-        {
+    for (auto r : std::views::iota(0, runs + warmupRuns)) {
+        if (r > 0) {
             GenerateRandomView(A);
             GenerateRandomView(B);
             GenerateRandomView(C);
