fa0ec5e6486bd366d475e676215e7e587b40789c
Prateek Chawla <p.chawla@fz-juelich.de>
2026-01-30 09:48:17 +0100

    updated main() with drivers
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -6,46 +6,84 @@
 #include "kernel.hpp"
 #include "profiler.hpp"
 
-int main(int argc, char* argv[]) {
+typedef void (*driven)(PerfEvent&, size_t&);
+
+typedef void (*driven_dynamic)(PerfEvent&, size_t&, size_t);
+
+void driver(driven drivee, std::string label = "", int runs = 100,
+            bool progress = true) {
   {
     // discard the first run as warmup
-    // PerfEvent monitor;
-    // kernel(monitor);
-    long long time;
-    kernel(time);
+    PerfEvent monitor;
+    size_t time;
+    drivee(monitor, time);
   }
 
-  for (auto i = 0; i < 10000; i++) {
-    // }
-    //   PerfEvent monitor("hwgroup.csv");
-    //   kernel(monitor);
-    //   auto report = monitor.GetReport(1.0, true);
-    //   // monitor.PrintReport(report);
-    //   DumpCSV("hw_data_2.csv", report);
-    // }
+  for (auto i = 0; i < runs; i++) {
+    {
+      PerfEvent monitor(label);
+      size_t time;
+      drivee(monitor, time);
+      auto report = monitor.GetReport(true);
+      // monitor.PrintReport(report);
+      DumpCSV(label + "_hwdata.csv", report);
+      DumpTimeToCSV(label + "_hwtime.csv", i, time);
+    }
 
-    // {
-    //   PerfEvent monitor("cachegroup.csv");
-    //   kernel(monitor);
-    //   auto report = monitor.GetReport(1.0, true);
-    //   // monitor.PrintReport(report);
-    //   DumpCSV("cache_data_2.csv", report);
-    // }
+    {
+      PerfEvent monitor("cachegroup.csv");
+      size_t time;
+      drivee(monitor, time);
+      auto report = monitor.GetReport(true);
+      // monitor.PrintReport(report);
+      DumpCSV(label + "_cachedata.csv", report);
+      DumpTimeToCSV(label + "_cachetime.csv", i, time);
+    }
 
-    // {
-    //   PerfEvent monitor("swgroup.csv");
-    //   kernel(monitor);
-    //   auto report = monitor.GetReport(1.0, true);
-    //   // monitor.PrintReport(report);
-    //   DumpCSV("sw_data_2.csv", report);
-    // }
-
-    long long time;
-    kernel(time);
-
-    DumpTimeToCSV("Rawtime.csv", i, time);
-
-    std::cout << "Completed " << i + 1 << "/1000 iterations. \r";
+    if (progress)
+      std::cout << "Completed " << i + 1 << "/" << runs << "iterations. \r";
   }
+}
+
+void driver_dyn(driven_dynamic drivee, std::string label = "",
+                int init_runs = 100, bool progress = true) {
+  for (size_t j = 1; j < init_runs; ++j) {
+    for (auto i = 0; i < 100; i++) {
+      {
+        PerfEvent monitor(label);
+        size_t time;
+        drivee(monitor, time, j);
+        auto report = monitor.GetReport(true);
+        // monitor.PrintReport(report);
+        DumpCSV(label + "_hwdata.csv", report);
+        DumpTimeToCSV(label + "_hwtime.csv", i, time);
+      }
+
+      {
+        PerfEvent monitor("cachegroup.csv");
+        size_t time;
+        drivee(monitor, time, j);
+        auto report = monitor.GetReport(true);
+        // monitor.PrintReport(report);
+        DumpCSV(label + "_cachedata.csv", report);
+        DumpTimeToCSV(label + "_cachetime.csv", j, time);
+      }
+    }
+
+    if (progress)
+      std::cout << "Completed " << j + 1 << "/" << init_runs
+                << "iterations. \r";
+  }
+}
+
+int main(int argc, char* argv[]) {
+  int runs = 1000;
+  // driver(dgemm_kernel, "datafiles/dgemm", runs, true);
+  // driver(ddot_kernel, "datafiles/ddot", runs, true);
+  // driver(fftw_kernel, "datafiles/fftw", runs, true);
+  // driver(sum_kernel, "datafiles/sum", runs, true);
+
+  driver_dyn(dynamic_sum_kernel, "datafiles/dyn_sum", runs, true);
+
   return 0;
 }
